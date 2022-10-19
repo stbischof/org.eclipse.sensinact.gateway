0854bdc14ad24f9f87864192d8eacbeb1ea6be97
Stefan Bischof <stbischof@bipolis.org>
2022-10-19 10:44:48 +0200

    mvn bnd-export:export -> /impl/target/tmp/export/feature-export/generated/feature-feature-export.json
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ prototype/tools/feature/src/main/java/org/eclipse/sensinact/gateway/tools/exporter/feature/internal/FeatureArtifact_.java
@@ -0,0 +1,23 @@
+package org.eclipse.sensinact.gateway.tools.exporter.feature.internal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.service.feature.FeatureArtifact;
+import org.osgi.service.feature.ID;
+
+public class FeatureArtifact_ implements FeatureArtifact {
+	public ID					id;
+	public Map<String, Object>	metadata	= new HashMap<>();
+
+	@Override
+	public ID getID() {
+		return id;
+	}
+
+	@Override
+	public Map<String, Object> getMetadata() {
+		return metadata;
+	}
+
+}
---
+++ prototype/tools/feature/src/main/java/org/eclipse/sensinact/gateway/tools/exporter/feature/json/FeatureExporterConfig.java
@@ -0,0 +1,8 @@
+package org.eclipse.sensinact.gateway.tools.exporter.feature.json;
+
+public class FeatureExporterConfig {
+	public boolean	structureDocumentation	= false;
+
+	public boolean	bundleHashes			= false;
+
+}
